4f675fa78ddc7ef7a92dad50e269b08822848cc3
Jodi Francis <jodiannfrancis3@gmail.com>
2020-03-05 17:34:03 -0800

    Final

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # CMSI 186 Lab 4 Robot Soccer Starter Code
-
-This repository contains an unfinished Java program that you are complete.
-
-Import this repository into a new repository of your own and write the code according to the lab [instructions](https://cs.lmu.edu/~ray/classes/plab/lab/4/).
-
-In your new repository, don’t forget to edit this README file, as _your_ repository will certainly not contain any unfinished Java programs.
+made a Soccer simulation in Java where the blue ball moves toward the goal. The red ball tries to push the blue ball back
--- RobotSoccerSimulation.java
+++ RobotSoccerSimulation.java
@@ -10,18 +10,17 @@ import javax.swing.SwingUtilities;
 public class RobotSoccerSimulation extends JPanel {
     private static final long serialVersionUID = -5228718339006830546L;
 
-    // The soccer field
-    private static double WIDTH = 400;
-    private static double HEIGHT = 600;
 
-    // Simulation parameters: in your lab, set these from command line arguments
-    private static double PLAYER_RADIUS = 15;
-    private static double ENEMY_RADIUS = 20;
-    private static double PLAYER_SPEED = 1.3;
-    private static double ENEMY_SPEED = 1.8;
-    private static double FRICTION = 0.0009;
+    private static final double WIDTH = 400;
+    private static final double HEIGHT = 600;
 
-    // Initially null; Set this to a string to end the simulation
+
+    private static final double PLAYER_RADIUS = 15;
+    private static final double ENEMY_RADIUS = 20;
+    private static final double PLAYER_SPEED = 1.9;
+    private static final double ENEMY_SPEED = 1.8;
+    private static final double FRICTION = 0.0009;
+
     private volatile String endMessage;
 
     static class Ball {
@@ -32,29 +31,38 @@ public class RobotSoccerSimulation extends JPanel {
         private Color color;
 
         Ball(double x, double y, double radius, double speed, Color color) {
-            // You know what to do here :)
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+            this.speed = speed;
+            this.color = color;
         }
 
         void moveToward(double targetX, double targetY) {
-            // Fill this in
+            var dx = targetX - this.x;
+            var dy = targetY- this.y;
+            var velocity = this.speed / Math.hypot(dx, dy);
+            this.x = constrain(this.x + velocity * dx, this.radius, WIDTH - this.radius);
+            this.y = constrain(this.y + velocity * dy, this.radius, HEIGHT -this.radius);
         }
 
-        // Slow down the ball by FRICTION. Don't let it go negative, though!
         void applyFriction() {
-            // Fill this in
+            this.speed = constrain(this.speed - FRICTION, 0, Double.POSITIVE_INFINITY);
         }
 
-        // Returns whether the ball is *entirely* inside the goal
         boolean inside(Goal goal) {
-            return false; // <--------- FIX THIS
+            return (this.x - this.radius > goal.x - goal.w / 2 &&
+                    this.x + this.radius < goal.x + goal.w / 2 &&
+                    this.y - this.radius > goal.y - goal.h / 2 &&
+                    this.y + this.radius < goal.y + goal.h / 2);
         }
     }
 
-    private static Ball[] balls = new Ball[] { 
+    private static Ball[] balls = new Ball[] {
         new Ball(0.0, HEIGHT, PLAYER_RADIUS, PLAYER_SPEED, Color.BLUE),
         new Ball(WIDTH * 0.25, 40, ENEMY_RADIUS, ENEMY_SPEED, Color.RED),
         new Ball(WIDTH * 0.75, 40, ENEMY_RADIUS, ENEMY_SPEED, Color.RED),
-        new Ball(WIDTH / 2, HEIGHT / 2, ENEMY_RADIUS, ENEMY_SPEED, Color.RED) 
+        new Ball(WIDTH / 2, HEIGHT / 2, ENEMY_RADIUS, ENEMY_SPEED, Color.RED)
     };
 
     private static class Goal {
@@ -66,7 +74,38 @@ public class RobotSoccerSimulation extends JPanel {
 
     private static Goal goal = new Goal();
 
-    // You don't need to touch this one.
+    private static double constrain(double input, double min, double max) {
+        return (Math.min(Math.max(input, min), max));
+    }
+
+    private static void adjustIfCollisions(Ball[] balls) {
+        for (var b1 : balls) {
+            for (var b2 : balls) {
+                if (b1 != b2) {
+                    var dx = b2.x - b1.x;
+                    var dy = b2.y - b1.y;
+                    final var distance = Math.hypot(dx, dy);
+                    var overlap = b1.radius + b2.radius - distance;
+                    if (overlap > 0) {
+                        final var adjustX = (overlap / 2) * (dx / distance);
+                        final var adjustY = (overlap / 2) * (dy / distance);
+                        b1.x -= adjustX;
+                        b1.y -= adjustY;
+                        b2.x += adjustX;
+                        b2.y += adjustY;
+                    }
+                }
+            }
+        }
+    }
+
+    private void endSimulationIfNecessary() {
+        if (balls[0].speed <= 0)
+            endMessage = "Oh no... try again";
+        if (balls[0].inside(goal))
+            endMessage = "GOAL";
+    }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         for (var ball : balls) {
@@ -85,14 +124,12 @@ public class RobotSoccerSimulation extends JPanel {
 
     private void runTheAnimation() {
         while (endMessage == null) {
-            //
-            // TODO: Update the state of the simulation.
-            //
-            // Note that it is okay, and in fact preferred, for you to add helper methods.
-            // For example, you should probably write private helper methods for collision
-            // detection and adjustment, and for ending the simulation by setting the
-            // proper endMessage.
-            //
+            for (int i = 0; i < balls.length; i++) {
+                balls[i].applyFriction();
+                balls[i].moveToward(i == 0? goal.x : balls[0].x, i == 0 ? goal.y : balls[0].y);
+            }
+            adjustIfCollisions(balls);
+            endSimulationIfNecessary();
             repaint();
             try {
                 Thread.sleep(10);
@@ -102,11 +139,12 @@ public class RobotSoccerSimulation extends JPanel {
     }
 
     public static void main(String[] args) {
+
         SwingUtilities.invokeLater(() -> {
             var panel = new RobotSoccerSimulation();
             panel.setBackground(Color.GREEN.brighter());
             var frame = new JFrame("Robotic Soccer");
-            frame.setSize(400, 600);
+            frame.setSize((int)WIDTH, (int)HEIGHT);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.getContentPane().add(panel, BorderLayout.CENTER);
             frame.setVisible(true);
